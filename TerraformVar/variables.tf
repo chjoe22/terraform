a033dab9ddc890126dd9ac3974131f6265f6ea2f
Vanden-Dummy <Vandspistol20@gmail.com>
2025-01-06 23:59:10 +0100

    Terraform snippet added from docs - more to come

---
+++ TerraformVar/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  description = "The project ID" // Project ID should be changed in terraform.tfvars
+  type        = string
+}
+
+variable "region" {
+  description = "The region" // Region should be changed in terraform.tfvars
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone" // Zone should be changed in terraform.tfvars
+  type        = string
+}
